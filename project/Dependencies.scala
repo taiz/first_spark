6777674624e27334ac59f96c55ef8c53e04f04dc
taiz <taiz7715@gmail.com>
2016-02-20 23:52:40 +0900

    update dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,13 +8,11 @@ object Version {
   val scalaTest    = "2.1.0"
   val mockito      = "1.9.5"
   val jets3t       = "0.9.4"
-  //val sparkDependencyScope = "provided
-  val sparkDependencyScope = "compile"
 }
 
 object Library {
-  val sparkCore      = "org.apache.spark"  %% "spark-core"      % Version.spark      % Version.sparkDependencyScope
-  val sparkSql       = "org.apache.spark"  %% "spark-sql"       % Version.spark      % Version.sparkDependencyScope
+  val sparkCore      = "org.apache.spark"  %% "spark-core"      % Version.spark
+  val sparkSql       = "org.apache.spark"  %% "spark-sql"       % Version.spark
   val hadoopClient   = "org.apache.hadoop" %  "hadoop-client"   % Version.hadoop
   val slf4jApi       = "org.slf4j"         %  "slf4j-api"       % Version.slf4j
   val logbackClassic = "ch.qos.logback"    %  "logback-classic" % Version.logback
